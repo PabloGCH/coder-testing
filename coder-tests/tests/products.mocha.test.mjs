58c90b7fa902058c54db4b10aea6c6fc71579214
PabloGCH <pablochoconi@gmail.com>
2023-02-19 18:01:42 -0300

    Mocha tests

--- coder-tests/tests/products.mocha.test.mjs
+++ coder-tests/tests/products.mocha.test.mjs
@@ -17,49 +17,6 @@ async function logIn() {
     return;
 }
 
-
-/*
-
-async function updateProduct(id :number|string) {
-    try {
-
-        console.log(response);
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-async function removeProduct(id :number|string) {
-    try {
-        let response = await axiosInstance.delete(`${BACKURL}/products/product/${id}`);
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-*/
-
-/*
-
-async function productsTest() {
-
-}
-async function test() {
-    //LOGS TO SERVER
-    await logIn();
-    await productsTest();
-    //await getProducts();
-    //let newProductId = await addProduct() || -1;
-    //await updateProduct(newProductId);
-    //await removeProduct(newProductId);
-}
-test();
-*/
-
-
-
 let id = -1;
 describe("LOGIN TEST", () => {
     it("USER WAS LOGGED", (done) => {
@@ -110,7 +67,42 @@ describe("ADD PRODUCT", () => {
     });
 });
 
+describe("UPDATE PRODUCT", () => {
+    let newProduct = {
+        name: "new name product",
+        price: 150,
+        imgUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.BcNb40Mq_PfOj795lDFqaAHaFV%26pid%3DApi&f=1&ipt=5d7140326ebeb108d2cb5cf625cd7be07f95cc4fcf3f908cc6f0c41d04e8c5b2&ipo=images" 
+    };
+    let response;
+    it("REQUEST WAS SUCCESFUL", (done) => {
+        axiosInstance.put(`${BACKURL}/products/product/${id}`, newProduct)
+            .then((res) => {
+                response = res;
+                assert.ok(response && response.data && response.data.success);
+                id = (response.data.newProduct._id || response.data.newProduct.id) || -1;
+                done();
+            })
+    });
+    it("PRODUCT WAS UPDATED WITH PROPER NAME", () => {
+        assert.ok(response.data && response.data.newProduct && response.data.newProduct.name === newProduct.name);
+    });
+    it("PRODUCT WAS UPDATED WITH PROPER PRICE", () => {
+        assert.ok(response.data && response.data.newProduct && response.data.newProduct.price === newProduct.price);
+    });
+    it("PRODUCT WAS UPDATED WITH PROPER IMGURL", () => {
+        assert.ok(response.data && response.data.newProduct && response.data.newProduct.imgUrl === newProduct.imgUrl);
+    });
+});
 
+describe("DELETE PRODUCT", () => {
+    it("REQUEST WAS SUCCESFUL", (done) => {
+        axiosInstance.delete(`${BACKURL}/products/product/${id}`)
+        .then((res) => {
+            assert.ok(res && res.data && res.data.success);
+            done();
+        });
+    });
+});
 
 
 
